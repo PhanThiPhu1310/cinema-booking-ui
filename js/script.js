5f55c23629949dc25da911e24f3f8fdc900caeeb
Phan Thi Phu <phuphanthi.work@gmail.com>
2025-01-06 20:34:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+// Get modal elements
 const modal = document.getElementById("modal");
 const modalImage = document.getElementById("modalImage");
 const captionText = document.getElementById("caption");
 const closeModal = document.querySelector(".close");
 
+// Get all images in the gallery
 const galleryItems = document.querySelectorAll(".gallery-item");
 
+// Add click event for each gallery image
 galleryItems.forEach((item) => {
   item.addEventListener("click", function () {
     modal.style.display = "flex";
@@ -13,10 +16,12 @@ galleryItems.forEach((item) => {
   });
 });
 
+// Close the modal when clicking on the "X" button
 closeModal.addEventListener("click", function () {
   modal.style.display = "none";
 });
 
+// Close the modal when clicking outside the image
 modal.addEventListener("click", function (event) {
   if (event.target === modal) {
     modal.style.display = "none";
